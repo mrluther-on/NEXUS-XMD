275f15224ae2fc67d1170ca6755c32f1671732f8
MR LUTHER <lutherofficial009@gmail.com>
2025-04-27 03:35:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "Pkdriller~MWsniQ6L#y2V86hoxGiNCLU0wIrc2fxRwP4IgwcOPWzjQo-DyQkw",
 // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
